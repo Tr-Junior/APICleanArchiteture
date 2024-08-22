8042d198d5754dff55f02d0cbd45f5eab51278be
Tr-Junior <tarcisio.junior1406@gmail.com>
2024-08-21 23:00:24 -0300

    update DIContainer.ts

--- src/infrastructure/DIContainer.ts
+++ src/infrastructure/DIContainer.ts
@@ -1,8 +1,11 @@
-import { InMemoryBookRepository } from "./repositories/InMemoryBookRepository";
+import { MongoBookRepository } from "./repositories/MongoBookRepository";
 import { GetAllBooks } from "../use-cases/GetAllBooks";
+import { CreateBook } from "../use-cases/CreateBook";
+import { UpdateBook } from "../use-cases/UpdateBook";
+import { DeleteBook } from "../use-cases/DeleteBook";
 
 class DIContainer {
-  private static _bookRepository = new InMemoryBookRepository();
+  private static _bookRepository = new MongoBookRepository();
 
   static getBookRepository() {
     return this._bookRepository;
@@ -10,6 +13,18 @@ class DIContainer {
 
   static getGetAllBooksUseCase() {
     return new GetAllBooks(this.getBookRepository());
+  }
+
+  static getCreateBookUseCase() {
+    return new CreateBook(this.getBookRepository());
+  }
+
+  static getUpdateBookUseCase() {
+    return new UpdateBook(this.getBookRepository());
+  }
+
+  static getDeleteBookUseCase() {
+    return new DeleteBook(this.getBookRepository());
   }
 }
 
