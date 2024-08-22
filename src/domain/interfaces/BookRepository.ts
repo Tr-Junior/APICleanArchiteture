13945154310194fbd9ef7d76f0821aae525cc7d9
Tr-Junior <tarcisio.junior1406@gmail.com>
2024-08-21 23:00:20 -0300

    update BookRepository.ts

--- src/domain/interfaces/BookRepository.ts
+++ src/domain/interfaces/BookRepository.ts
@@ -4,6 +4,6 @@ export interface BookRepository {
   findAll(): Promise<Book[]>;
   findById(id: string): Promise<Book | null>;
   create(book: Book): Promise<Book>;
-  update(book: Book): Promise<void>;
+  update(book: Book): Promise<Book | null>; 
   delete(id: string): Promise<void>;
 }
