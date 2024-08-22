bd816b3aa3586ecf10c1efea110b68adceb3c1d5
Tr-Junior <tarcisio.junior1406@gmail.com>
2024-08-21 23:01:03 -0300

    update GetAllBooks.ts

--- src/use-cases/GetAllBooks.ts
+++ src/use-cases/GetAllBooks.ts
@@ -1,9 +1,10 @@
 import { BookRepository } from "../domain/interfaces/BookRepository";
+import { Book } from "../domain/entities/Book";
 
 export class GetAllBooks {
   constructor(private bookRepository: BookRepository) {}
 
-  async execute() {
+  async execute(): Promise<Book[]> {
     return await this.bookRepository.findAll();
   }
 }
